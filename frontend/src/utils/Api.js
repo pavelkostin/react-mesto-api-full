f938c4331f58b559011bb80456faa8e199681334
pavelkostin <pasha.kostin@yandex.ru>
2021-12-27 19:11:30 +0300

    15 работа

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,7 +1,7 @@
 class Api {
     constructor(options) {
         this._url = options.url;
-        this._headers = options.headers;
+        /* this._headers = options.headers; */
     }
 
     _checkResponse(res) {
